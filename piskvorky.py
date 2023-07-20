da2a3524fc70d67c235f5ecfe96150fad6579d94
Honzikova-Cesta <125808309+Honzikova-Cesta@users.noreply.github.com>
2023-07-20 11:21:26 +0200

    verze2

--- piskvorky.py
+++ piskvorky.py
@@ -36,7 +36,7 @@ position = [n for n in range(9)]
 grid_line = '+---+---+---+'
 
 
-#3x3 grid
+#3x3 grid napsat ve for cyklu?
 print(grid_line)
 print(f'|{position[0]:^{3}}|{position[1]:^{3}}|{position[2]:^{3}}|')
 print(grid_line)
@@ -48,14 +48,29 @@ print(grid_line)
 vyherni_pozice = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 kontrola_x = []
 kontrola_o = []
-
+obsazena_pozice = []
 n=True
 while n:
-   
-    mark_x = int(input('Player X turn, choose position with number: '))
-    kontrola_x.append(mark_x)
-    position[mark_x] = 'x'
 
+    #HRAC - x -
+    while True:   
+        try:
+            mark_x = int(input('Player X turn, choose position with number: '))
+            if mark_x in obsazena_pozice:
+                print('tato pozice je obsazena')
+            else:
+                obsazena_pozice.append(mark_x)
+                kontrola_x.append(mark_x)
+                position[mark_x] = 'x'
+                break
+        except ValueError:
+            print('spatna hodnota')
+        
+
+    
+    
+
+    #grid
     print(grid_line)
     print(f'|{position[0]:^{3}}|{position[1]:^{3}}|{position[2]:^{3}}|')
     print(grid_line)
@@ -63,18 +78,31 @@ while n:
     print(grid_line)
     print(f'|{position[6]:^{3}}|{position[7]:^{3}}|{position[8]:^{3}}|')
     print(grid_line)
-    #kontrola vyhernich pozic = 0 1 2, 3 4 5, 6 7 8, 0 3 6, 1 4 7, 2 5 8, 0 4 8, 2 4 6
     
+    #vyherni pozice = 0 1 2, 3 4 5, 6 7 8, 0 3 6, 1 4 7, 2 5 8, 0 4 8, 2 4 6
     if kontrola_x in vyherni_pozice:
         print('X is the winner!')
         n=False
-
+    
+    print(obsazena_pozice)
     print(kontrola_x)
 
-    mark_o = int(input('Player O turn, choose position with number: '))
-    kontrola_o.append(mark_o)
-    position[mark_o] = 'o'
-
+    #HRAC - o -
+    
+    while True:   
+        try:
+            mark_o = int(input('Player O turn, choose position with number: '))
+            if mark_o in obsazena_pozice:
+                print('tato pozice je obsazena')
+            else:
+                obsazena_pozice.append(mark_o)
+                kontrola_o.append(mark_o)
+                position[mark_o] = 'o'
+                break
+        except ValueError:
+            print('spatna hodnota')
+    
+    #grid
     print(grid_line)
     print(f'|{position[0]:^{3}}|{position[1]:^{3}}|{position[2]:^{3}}|')
     print(grid_line)
@@ -82,9 +110,11 @@ while n:
     print(grid_line)
     print(f'|{position[6]:^{3}}|{position[7]:^{3}}|{position[8]:^{3}}|')
     print(grid_line)
-   
+    
+    #vyherni pozice = 0 1 2, 3 4 5, 6 7 8, 0 3 6, 1 4 7, 2 5 8, 0 4 8, 2 4 6   
     if kontrola_o in vyherni_pozice:
         print('O is the winner!')
         n=False
-
+    
+    print(obsazena_pozice)
     print(kontrola_o)
